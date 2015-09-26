8fb8debc3a467f70f0427415cb5806a5d57a769b
mhunger <m.hunger@yahoo.de>
2015-09-26 18:24:43 +0200

    Added first game repository
    - added game repository interface, so that the games can now be retrieved
    - Game representation does not yet use HATEOAS properly for the teams

---
+++ api/src/main/java/org/hungerhome/soccermanager/api/repository/GameRepository.java
@@ -0,0 +1,6 @@
+package org.hungerhome.soccermanager.api.repository;
+
+import org.hungerhome.soccermanager.api.entity.Game;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameRepository extends CrudRepository<Game, Integer> { }
