d5d7729a890e7aa91f1c1104f93aa2c65d04fdcb
mhunger <m.hunger@yahoo.de>
2015-09-26 18:21:52 +0200

    Adding configuration annotation
    - added configuration annotation, so that rest mvc config is taken

--- api/src/main/java/org/hungerhome/soccermanager/api/config/SoccerManagerRestMvcConfiguration.java
+++ api/src/main/java/org/hungerhome/soccermanager/api/config/SoccerManagerRestMvcConfiguration.java
@@ -1,8 +1,10 @@
 package org.hungerhome.soccermanager.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
+@Configuration
 public class SoccerManagerRestMvcConfiguration extends RepositoryRestMvcConfiguration
 {
     @Override
