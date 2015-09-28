e393f8e86356638605a60f170be3bf17ee443e74
mhunger <m.hunger@yahoo.de>
2015-09-28 10:38:41 +0200

    Fix ID type in Game API
    - changed type in game repo to Long, which is standard id type for sql based dbs

--- api/src/main/java/org/hungerhome/soccermanager/api/repository/GameRepository.java
+++ api/src/main/java/org/hungerhome/soccermanager/api/repository/GameRepository.java
@@ -3,4 +3,4 @@ package org.hungerhome.soccermanager.api.repository;
 import org.hungerhome.soccermanager.api.entity.Game;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GameRepository extends CrudRepository<Game, Integer> { }
+public interface GameRepository extends CrudRepository<Game, Long> { }
---
+++ db/0004_change_date_of_game_to_not_null.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `game` CHANGE `date` `date` DATETIME  NOT NULL;
