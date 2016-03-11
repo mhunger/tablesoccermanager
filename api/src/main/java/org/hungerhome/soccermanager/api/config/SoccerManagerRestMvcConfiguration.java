c2dcc042cfd602e77f29a5f7b4b4f2b7f352929b
mhunger <m.hunger@yahoo.de>
2016-03-11 01:39:29 +0100

    Added Bean dataSource for db test fixture loading
    - created a bean in config class that returns DriverMaagerDataSource
    - Required by Sprint Test to load xml files

--- api/src/main/java/org/hungerhome/soccermanager/api/config/SoccerManagerRestMvcConfiguration.java
+++ api/src/main/java/org/hungerhome/soccermanager/api/config/SoccerManagerRestMvcConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class SoccerManagerRestMvcConfiguration extends RepositoryRestMvcConfiguration
@@ -20,4 +21,15 @@ public class SoccerManagerRestMvcConfiguration extends RepositoryRestMvcConfigur
     PlayerEventHandler playerEventHandler() {
         return new PlayerEventHandler();
     }
+
+    @Bean
+    DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://soccer.dev/soccermanager_test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
 }
